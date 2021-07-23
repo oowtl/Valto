3b8b4760894bd8a084023506b8990690f66ddec6
cember123 <cember123@gmail.com>
2021-07-23 14:03:03 +0900

    test

--- frontend/src/views/main/main.vue
+++ frontend/src/views/main/main.vue
@@ -4,18 +4,12 @@
       :height="`70px`"
       @openLoginDialog="onOpenLoginDialog"
       @openJoinDialog="onOpenJoinDialog"
-<<<<<<< HEAD
       @openCreateRoomDialog="onOpenCreateRoomDialog"
       />
-<<<<<<< HEAD
 
-
-=======
->>>>>>> ef2d194a3ebeed4bf6c0459a0cbc0a9e294e14cf
-=======
       @openProfileDialog="onOpenProfileDialog"
       />
->>>>>>> 2e5b746cc00bf59bfde1a2337c4d392807fdd8d6
+
     <el-container class="main-container">
       <el-aside class="hide-on-small" width="240px">
         <main-sidebar
@@ -29,23 +23,13 @@
   </el-container>
   <login-dialog
     :open="loginDialogOpen"
-    @closeLoginDialog="onCloseLoginDialog"
-  />
+    @closeLoginDialog="onCloseLoginDialog"/>
   <join-dialog
     :open="joinDialogOpen"
-<<<<<<< HEAD
     @closeJoinDialog="onCloseJoinDialog"/>
   <createroom-dialog
     :open="createRoomDialogOpen"
     @closeCreateRoomDialog="onCloseCreateRoomDialog"/>
-=======
-    @closeJoinDialog="onCloseJoinDialog"
-  />
-  <profile-dialog
-    :open="profileDialogOpen"
-    @closeProfileDialog="onCloseProfileDialog"
-  />
->>>>>>> 2e5b746cc00bf59bfde1a2337c4d392807fdd8d6
 </template>
 <style>
   @import "https://unpkg.com/element-plus/lib/theme-chalk/index.css";
@@ -60,11 +44,7 @@ import MainHeader from './components/main-header'
 import MainSidebar from './components/main-sidebar'
 import MainFooter from './components/main-footer'
 import JoinDialog from './components/join-dialog'
-<<<<<<< HEAD
 import CreateroomDialog from './components/createroom-dialog'
-=======
-import ProfileDialog from './components/profile-dialog'
->>>>>>> 2e5b746cc00bf59bfde1a2337c4d392807fdd8d6
 
 export default {
   name: 'Main',
@@ -74,29 +54,13 @@ export default {
     MainFooter,
     LoginDialog,
     JoinDialog,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     CreateroomDialog
->>>>>>> ef2d194a3ebeed4bf6c0459a0cbc0a9e294e14cf
-=======
-    ProfileDialog,
->>>>>>> 2e5b746cc00bf59bfde1a2337c4d392807fdd8d6
   },
   data () {
     return {
       loginDialogOpen: false,
       joinDialogOpen: false,
-<<<<<<< HEAD
       localLoginFlag: false,
-<<<<<<< HEAD
-=======
-      createRoomDialogOpen: false
->>>>>>> ef2d194a3ebeed4bf6c0459a0cbc0a9e294e14cf
-=======
-      profileDialogOpen: false,
-      localLoginFlag: false,
->>>>>>> 2e5b746cc00bf59bfde1a2337c4d392807fdd8d6
     }
   },
   methods: {
@@ -112,21 +76,12 @@ export default {
     onCloseJoinDialog () {
       this.joinDialogOpen = false
     },
-<<<<<<< HEAD
     onOpenCreateRoomDialog () {
       this.createRoomDialogOpen = true
     },
     onCloseCreateRoomDialog () {
       this.createRoomDialogOpen = false
     }
-=======
-    onOpenProfileDialog () {
-      this.profileDialogOpen = true
-    },
-    onCloseProfileDialog () {
-      this.profileDialogOpen = false
-    },
->>>>>>> 2e5b746cc00bf59bfde1a2337c4d392807fdd8d6
   }
 }
 </script>
