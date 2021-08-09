79bfd1a4fd3d5f5acbdb002a90f5f65ba28844f8
dongyun <hoodiesparrow@gmail.com>
2021-08-09 14:06:56 +0900

    [S05P13D103-62] front/방 WebRTC : 버그수정 detail dialog

--- frontend/src/views/room/room.vue
+++ frontend/src/views/room/room.vue
@@ -75,39 +75,27 @@ export default{
 
       // 토큰과 클라이언트의 정보를 전달하며 세션에 연결함
       const token = localStorage.getItem('st')
-          state.session.connect(token, { clientData: state.nickname })
-					.then(() => {
-            // --- Get your own camera stream with the desired properties ---
-						let publisher = state.OV.initPublisher(undefined, {
-              audioSource: undefined, // The source of audio. If undefined default microphone
-							videoSource: undefined, // The source of video. If undefined default webcam
-							publishAudio: true,  	// Whether you want to start publishing with your audio unmuted or not
-							publishVideo: true,  	// Whether you want to start publishing with your video enabled or not
-							resolution: '640x480',  // The resolution of your video
-							frameRate: 30,			// The frame rate of your video
-							insertMode: 'APPEND',	// How the video is inserted in the target element 'video-container'
-							mirror: false       	// Whether to mirror your local video or not
-						})
-            state.mainStreamManager = publisher
-
-            console.log('room onBeforeMount token state.mainStreamManager : ' + state.mainStreamManager)
-
-						state.publisher = publisher
-            console.log(state.OV)
-
-            // console.log(state.mainStreamManager)
-            // console.log(publisher)
-            // console.log(state.publisher)
-            // When our HTML video has been added to DOM... 부분(jsjava예제 85줄 >>> 생략됨. nickname과 username부분 확인 필요)
-            // 8. Publish your stream
-            // console.log(publisher)
-            // console.log(state.publisher)
-            state.session.publish(state.publisher)
-
+      // state.session.connect(token, { clientData: state.nickname })
+      state.session.connect(token, {})
+        .then(() => {
+          // --- Get your own camera stream with the desired properties ---
+          let publisher = state.OV.initPublisher(undefined, {
+            audioSource: undefined, // The source of audio. If undefined default microphone
+            videoSource: undefined, // The source of video. If undefined default webcam
+            publishAudio: true,  	// Whether you want to start publishing with your audio unmuted or not
+            publishVideo: true,  	// Whether you want to start publishing with your video enabled or not
+            resolution: '640x480',  // The resolution of your video
+            frameRate: 30,			// The frame rate of your video
+            insertMode: 'APPEND',	// How the video is inserted in the target element 'video-container'
+            mirror: false       	// Whether to mirror your local video or not
           })
-          .catch(error => {
-            console.log('There was an error connecting to the session:', error.code, error.message);
-					})
+          state.mainStreamManager = publisher
+          state.publisher = publisher
+          state.session.publish(state.publisher)
+        })
+        .catch(error => {
+          console.log('There was an error connecting to the session:', error.code, error.message);
+        })
     })
 
     //세션 나가기
