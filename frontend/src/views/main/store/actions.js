ce00f5734d6427982eaad55bae80ac8d6c4ee7a5
maru9525 <cember123@gmail.com>
2021-08-13 14:34:18 +0900

    ㅅㄷㄴㅅ 4

--- frontend/src/views/main/store/actions.js
+++ frontend/src/views/main/store/actions.js
@@ -137,7 +137,7 @@ export function requestDetail({ state }, payload) {
 
 export function requestRoomToken({ state }, payload) {
   // 추후 'api/v1/room/{roomId}/admission' 으로 변경
-  const url = 'https://localhost:8443/api/sessions/get-token'
+  const url = '/api/sessions/get-token'
   const body = { sessionName: `${payload}` }
   const request = {
     method: 'post',
